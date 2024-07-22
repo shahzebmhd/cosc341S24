5c71a9846a61acc2997bb2315eece9b81bfe6b7a
Ravi <ravi@Ravis-MacBook-Pro.local>
2024-07-21 22:10:44 -0700

    updates on the assignment with script updates

---
+++ Assignment 1/Scripts/Player.cs
@@ -0,0 +1,63 @@
+ using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    [SerializeField] private Transform groundCheckTransform = null;
+    private bool jumpKeyWasPressed;
+    private float horizontalInput;
+    private Rigidbody rigidbodyComponent;
+    [SerializeField]private LayerMask playerMask;
+    
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigidbodyComponent = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Check if space key is pressed down
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+            jumpKeyWasPressed = true;
+        }
+
+        horizontalInput = Input.GetAxis("Horizontal");
+        
+
+
+       
+    }
+    // FixedUpdate is called once every physic update 
+    private void FixedUpdate()
+    {
+        rigidbodyComponent.velocity = new Vector3(horizontalInput, rigidbodyComponent.velocity.y, 0);
+
+        if(Physics.OverlapSphere(groundCheckTransform.position, 0.1f, playerMask).Length == 0)
+        {
+            return;
+        }
+       
+        if(jumpKeyWasPressed)
+        {
+            rigidbodyComponent.AddForce(Vector3.up * 7, ForceMode.VelocityChange);
+            jumpKeyWasPressed = false;
+        }
+
+        
+
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.layer == 9)
+        {
+            Destroy(other.gameObject);
+        }
+    }
+
+}
